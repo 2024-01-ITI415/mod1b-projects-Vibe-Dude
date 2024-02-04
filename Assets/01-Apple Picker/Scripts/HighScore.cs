0c52128e99b9f074088373f4982f69282127d8ab
Vibe-Dude <7000chuck@gmail.com>
2024-02-04 17:12:34 -0500

    Finished Base Game

---
+++ Assets/01-Apple Picker/Scripts/HighScore.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI; // Remember, we need this line for uGUI to work.
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+    private TextMeshProUGUI text;
+    void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+      // If the PlayerPrefs HighScore already exists, read it
+        if (PlayerPrefs.HasKey("HighScore"))
+        { 
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        // Assign the high score to HighScore
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+    void Update()
+    { 
+        //Text gt = this.GetComponent<Text>();
+        text.SetText("High Score: " + score);
+        // Update the PlayerPrefs HighScore if necessary
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        { // d
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+}
